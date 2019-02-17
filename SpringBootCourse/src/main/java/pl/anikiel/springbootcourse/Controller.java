0e8b1ad9c9576d91714b8d8f0d87720cd4f924b6
Adrian Nikiel <hunter86@onet.eu>
2019-02-17 21:12:15 +0100

    added first user and controller

---
+++ SpringBootCourse/src/main/java/pl/anikiel/springbootcourse/Controller.java
@@ -0,0 +1,16 @@
+package pl.anikiel.springbootcourse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class Controller {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        User user = new User();
+        user.setFirstName("Adrian");
+        return "Hello End User";
+    }
+}
