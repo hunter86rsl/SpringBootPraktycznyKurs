1e3abef7b5f63443a032a797d51e5e1face210dc
Adrian Nikiel <hunter86@onet.eu>
2019-03-03 21:33:38 +0100

    added UserController

---
+++ SpringBootCourse/src/main/java/pl/anikiel/springbootcourse/UserController.java
@@ -0,0 +1,31 @@
+package pl.anikiel.springbootcourse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(value = "/registry", method = RequestMethod.POST)
+    public User registry(@RequestBody User user) {
+        return user;
+    }
+
+    @RequestMapping(value = "/confirm/{id}", method = RequestMethod.PUT)
+    public User confirm(@PathVariable(value = "id") Long id) {
+        return new User();
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.PUT)
+    public User edit(@RequestBody User user) {
+        return user;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.PUT)
+    public User add(@RequestBody User user) {
+        return user;
+    }
+
+}
